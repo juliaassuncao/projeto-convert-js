116200832f82d7f6dbb1df3c5335ee99b7f67888
Julia Assunção <juliaassuncao256@gmail.com>
2025-03-19 17:23:54 -0300

    Obtendo o valor digitado no input

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-console.log("JS conectado!")
+const amount = document.getElementById("amount")
+
+// Manipulando o input amount para receber somente números
+amount.addEventListener("input", () => {
+    console.log(amount.value)
+})
